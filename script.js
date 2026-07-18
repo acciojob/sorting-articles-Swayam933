317afd9cb72456c13d9f6df5e230bcbc684f9e10
Chandan Kumar Pradhan <swayampradhan24@gmail.com>
2026-07-18 23:06:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-//your JS code here. If required.
+
+const bands = ['The Plot in You', 'The Devil Wears Prada', 'Pierce the Veil', 'Norma Jean', 'The Bled', 'Say Anything', 'The Midway State', 'We Came as Romans', 'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 'Anywhere But Here', 'An Old Dog'];
+
+const articles = ['a', 'an', 'the'];
+
+function sortKey(name) {
+  const words = name.split(' ');
+  if (articles.includes(words[0].toLowerCase())) {
+    return words.slice(1).join(' ').toLowerCase();
+  }
+  return name.toLowerCase();
+}
+
+const sortedBands = [...bands].sort((a, b) => sortKey(a).localeCompare(sortKey(b)));
+
+const list = document.getElementById('band');
+sortedBands.forEach(name => {
+  const li = document.createElement('li');
+  li.textContent = name;
+  list.appendChild(li);
+});
